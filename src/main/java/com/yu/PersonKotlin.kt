a6c203197bc1b6613be6d97c120b4fae1214be29
Yuchun0221 <hyc1327@gmail.com>
2022-06-10 16:17:02 +0800

    Hello world kotlin

---
+++ src/main/java/com/yu/PersonKotlin.kt
@@ -0,0 +1,16 @@
+package com.yu
+
+class PersonKotlin {
+    fun greeting(){
+        println("hello kotlin")
+    }
+}
+//Top-level
+fun main() {
+    val p = PersonKotlin()
+    p.greeting()
+
+}
+fun main(args: Array<String>) {
+
+}
