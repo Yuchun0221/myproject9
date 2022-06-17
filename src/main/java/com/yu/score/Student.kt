05748dce445bb44c0bf54e346c7f16a5a67df583
Yuchun0221 <hyc1327@gmail.com>
2022-06-17 15:04:29 +0800

    kotlin

---
+++ src/main/java/com/yu/score/Student.kt
@@ -0,0 +1,23 @@
+package com.yu.score
+
+class Student (var name:String, var english:Int, var math:Int){
+    init {
+        println("Testing")
+    }
+    fun print(){
+//        println(name+"\t"+english+"\t"+math)
+        println("$name\t$english\t$math\t${getAverage()}")
+    }
+    fun getAverage():Int{
+        return (english+math)/2
+    }
+/*    var name = " "
+    var english = 0
+    var math = 0*/
+}
+fun main(){
+    val stu = Student("Jack",55,60)
+    val stu1 = Student("Tom",80,80)
+    println(stu.english)
+    println(stu1.math)
+}
