50169b616c88cef660b339e9e8c9a895970f4233
Yuchun0221 <hyc1327@gmail.com>
2022-06-17 15:59:40 +0800

    score

--- src/main/java/com/yu/score/Student.kt
+++ src/main/java/com/yu/score/Student.kt
@@ -8,16 +8,19 @@ class Student (var name:String, var english:Int, var math:Int){
 //        println(name+"\t"+english+"\t"+math)
         println("$name\t$english\t$math\t${getAverage()}")
     }
-    fun getAverage():Int{
+/*    fun getAverage():Int{
         return (english+math)/2
-    }
+    }*/
+    fun getAverage()=(english+math)/2
 /*    var name = " "
     var english = 0
     var math = 0*/
 }
 fun main(){
     val stu = Student("Jack",55,60)
-    val stu1 = Student("Tom",80,80)
+    val stu1 = Student("Hank",80,80)
+    stu.print()
+    stu1.print()
     println(stu.english)
     println(stu1.math)
 }
